67fbe0f0a09fb84f3b6c69da0d058fd57ac3cf4e
Giacomo Marciani <giacomo.marciani@gmail.com>
2017-05-21 21:44:03 +0200

    word counter

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# SABD 2016 - BATCH
+# MOVIEDOOP
 
-*A batch processing application.*
+*A batch processing application for movie analytics, leveraging Hadoop.*
 
 *Coursework in Systems and Architectures for Big Data 2016/2017*
 
@@ -10,10 +10,9 @@ Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu 
 Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 
 ## Installation
-Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+The system needs to be provided with the following:
+* Hadoop v3.0.0.0-alpha2
+* Java 1.8
 
 ## Usage
 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
---
+++ src/main/java/com/acmutv/moviedoop/map/TokenizationMapper.java
@@ -0,0 +1,70 @@
+/*
+  The MIT License (MIT)
+
+  Copyright (c) 2017 Giacomo Marciani and Michele Porretta
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+
+  The above copyright notice and this permission notice shall be included in
+  all copies or substantial portions of the Software.
+
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+  THE SOFTWARE.
+ */
+package com.acmutv.moviedoop.map;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+/**
+ * The mapper for the {@code WordCount} job.
+ *
+ * @author Giacomo Marciani {@literal <gmarciani@acm.org>}
+ * @author Michele Porretta {@literal <mporretta@acm.org>}
+ * @since 1.0
+ */
+public class TokenizationMapper  extends Mapper<Object,Text,Text,IntWritable> {
+
+  /**
+   * The count to emit.
+   */
+  private static final IntWritable ONE = new IntWritable(1);
+
+  /**
+   * The word to emit.
+   */
+  private Text word = new Text();
+
+  /**
+   * The mapping routine.
+   *
+   * @param key the input key.
+   * @param value the input value.
+   * @param ctx the context.
+   * @throws IOException when the context cannot be written.
+   * @throws InterruptedException when the context cannot be written.
+   */
+  public void map(Object key, Text value, Context ctx) throws IOException, InterruptedException {
+    StringTokenizer tokenizer = new StringTokenizer(value.toString());
+    while (tokenizer.hasMoreTokens()) {
+      this.word.set(tokenizer.nextToken());
+      ctx.write(this.word, ONE);
+    }
+  }
+}
